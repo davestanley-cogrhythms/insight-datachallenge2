6b9ba844438a951409f2055424a99e626fb172d9
David Stanley <davearthur.stanley@gmail.com>
2019-02-28 14:40:09 -0500

    Added additional tutorial info for postgres

--- setup_yammer_db.py
+++ setup_yammer_db.py
@@ -1,31 +1,27 @@
-## Python packages - you may have to pip install sqlalchemy, sqlalchemy_utils, and psycopg2.
+## Python packages
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 import psycopg2
 import pandas as pd
 
-#In Python: Define your username and password used above. I've defined the database name (we're
-#using a dataset on births, so I call it birth_db).
+#Define username and db name
 dbname = 'yammer_db'
 username = 'davestanley'
 pswd = 'coco'
 
-## 'engine' is a connection to a database
-## Here, we're using postgres, but sqlalchemy can connect to other things too.
+# Connect to postgres database. sqlalchemy can connect to other db's as well
 engine = create_engine('postgresql://%s:%s@localhost/%s'%(username,pswd,dbname))
 print('postgresql://%s:%s@localhost/%s'%(username,pswd,dbname))
 print(engine.url)
-# Replace localhost with IP address if accessing a remote server
 
-## create a database (if it doesn't exist)
+## create db
 if not database_exists(engine.url):
     create_database(engine.url)
 print(database_exists(engine.url))
 print(engine.url)
 
 
-# load a database from the included CSV
-# edit the string below to account for where you saved the csv.
+# load db from csv and send to SQL
 import os
 
 csv_path = os.path.join('.','yammer_events.csv')
